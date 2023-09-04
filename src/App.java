5db590322ba91672d49377a47982aedf8bda2035
kvdl <evkachmala@gmail.com>
2023-09-04 17:24:33 +0700

    added choose

--- src/App.java
+++ src/App.java
@@ -4,10 +4,20 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         final String password;
+        final boolean encrypt;
+        final File file;
         try (Scanner input = new Scanner(System.in);) {
+            System.out.print("Введите зашифровать или расшифровать(y/n) --> ");
+            encrypt = input.nextLine().equals("y");
             System.out.print("Введите ключ шифрования --> ");
             password = input.nextLine();
+            System.out.print("Введите путь к файлу --> ");
+            file = new File(input.nextLine());
         }
-        Scheta.decryptFile(new File("video.mp4"), 2048, password);
+        if (encrypt) {
+            Scheta.encryptFile(file, 2048, password);
+        } else {
+            Scheta.decryptFile(file, 2048, password);
+        }
     }
 }
